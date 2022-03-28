f266f33107508528c45249840e68174339fbd521
MKDEUX <77662906+MKDEUX@users.noreply.github.com>
2022-03-28 21:57:52 +0200

    Add files via upload

---
+++ TRS2/array.php
@@ -0,0 +1,16 @@
+<?php 
+
+$tab   = [0 =>"john", 1 =>"paul",2=>"ringo"];
+// print_r($tab);
+
+
+$covid =['paul'=> true , 'léo'=> false , 'tobby'=> true ];
+// print_r($covid);
+
+$tabtab=["tom"=>[12,15,18],"léa"=>[5,12,9],"roger"=>[8,14,16]];
+// print_r($tabtab["tom"][1]);
+
+$ville=["lyon","milan","tunis","grenoble"];
+print_r($ville);
+
+?>
